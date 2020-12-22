bc98f097ebfa465a617255930c568be5cc9d7d15
aryanpa <75675640+aryanpa@users.noreply.github.com>
2020-12-22 22:02:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,66 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-var engine, world, obj, ball;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
 function setup() {
-  createCanvas(400,400);
-  
-  engine = Engine.create();
-  world = engine.world;
-  var obj_options = {
-    isStatic:true
-  }
-  obj = Bodies.rectangle(200,390,200,20,obj_options);
-  World.add(world,obj);
-  console.log(obj.position.x);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  var ball_options = {
-    restitution:1.0
-  }
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-  ball = Bodies.circle(200,100,20,ball_options);
-  World.add(world,ball);
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:false});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
   
 }
+
 
 function draw() {
-  background(0); 
-  Engine.update(engine); 
- 
   rectMode(CENTER);
-  rect(obj.position.x,obj.position.y,400,20);
-   
-  ellipseMode(RADIUS);
-  ellipse(ball.position.x, ball.position.y,20,20);
-
-
-
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
 }
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
+    
+  }
+}
+
+
+
